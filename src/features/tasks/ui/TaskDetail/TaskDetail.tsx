f7c339f1663b47e2e69993271eded8692e4a1d0d
Anna <blinova.anneta@gmail.com>
2025-09-08 10:40:05 +0200

    `feat: Add task detail caching and refresh logic`

--- src/features/tasks/ui/TaskDetail/TaskDetail.tsx
+++ src/features/tasks/ui/TaskDetail/TaskDetail.tsx
@@ -9,13 +9,22 @@ type Props = {
   selectedTaskId: string | null | undefined;
 }
 
+type CachedItem<T> = {
+  value: T;              // сами данные
+  expirationDate: number // timestamp, когда данные устареют
+}
+
 export const TaskDetail = ({selectedTaskId, boardId}: Props) => {
   const [fullTask, setFullTask] = useState<TaskDetailResponse | null>(null);
   const [detailsQueryStatus, setDetailsQueryStatus] = useState<'loading' | 'success' | 'error' | 'idle'>('idle');
   const abortControllerRef = useRef<null | AbortController>(null)
 
+  const cacheData = useRef<Record<string, CachedItem<TaskDetailResponse>>>({})
+
   useEffect(() => {
-    setDetailsQueryStatus('loading')
+
+    // Отменяем предыдущий запрос
+    abortControllerRef.current?.abort()
 
     if(!selectedTaskId || !boardId) {
       setFullTask(null)
@@ -23,19 +32,35 @@ export const TaskDetail = ({selectedTaskId, boardId}: Props) => {
       return
     }
 
-    setDetailsQueryStatus('loading')
+    const cachedItem = cacheData.current[`cached-${selectedTaskId}`]
+    // Если в кэше есть свежие данные
+    if(cachedItem && cachedItem.expirationDate > Date.now()) {
+      // Данные свежие — можно использовать
+      setFullTask(cachedItem.value)
+      return
+    }
 
-    // Отменяем предыдущий запрос
-    abortControllerRef.current?.abort()
+    // Если данные устарели — удаляем их
+    if(cachedItem) {
+      // Данные протухли — удаляем их из кэша
+      delete cacheData.current[`cached-${selectedTaskId}`]
+    }
+
     // Создаём новый AbortController для нового запроса
     const abortController = new AbortController()
     abortControllerRef.current = abortController
+
+    setDetailsQueryStatus('loading')
 
     tasksApi.getTaskDetails(selectedTaskId, boardId, abortController.signal)
       .then((data) => {
         // console.log('Task detail response', data);
         setFullTask(data);
         setDetailsQueryStatus('success')
+        cacheData.current[`cached-${selectedTaskId}`] = {
+          value: data,
+          expirationDate: Date.now() + 60 * 1000,
+        }
       })
       .catch((error) => {
         console.error('Error fetching task details:', error);
